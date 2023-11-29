d2c7de17bdb949f56ce05ae4332ede2ce848bd79
ananafrida <anan.afrida.yes@gmail.com>
2023-11-28 21:01:42 -0500

    delete git page

--- build/static/js/510.f5060375.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkanan=self.webpackChunkanan||[]).push([[510],{510:function(e,n,s){s.r(n);var a=s(184);n.default=function(){return(0,a.jsx)("div",{className:"blog",children:(0,a.jsx)("p",{children:"\xa9 Copyright 2022, GRUVVE. All Rights Reserved."})})}}}]);
-//# sourceMappingURL=510.f5060375.chunk.js.map
